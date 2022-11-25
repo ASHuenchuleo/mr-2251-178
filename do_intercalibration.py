6e3033f22daa3a4ff3835651ecb919087dabca77
Angela Sepulveda <angela@yeti.das.uchile.cl>
2022-11-25 10:01:06 -0300

    Donde u calibration

---
+++ do_intercalibration.py
@@ -0,0 +1,11 @@
+from inter_calibration import InterCalibrator
+csv = 'AVA_MR_2251-178_lco.csv'
+filters = ['u', 'B', 'g', 'V', 'r', 'i', 'z']
+objName = 'MR_2251'
+
+cal = InterCalibrator(csv, objName)
+priors = [[0.01, 10.0], [0.0, 2.0]]
+Nsamples=15000
+Nburnin=10000
+f = filters[3]
+cal.calibrate(f, priors=priors, Nsamples=Nsamples, Nburnin=Nburnin)
